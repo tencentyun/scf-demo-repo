05f70f118eacece538a60930aa01246f935375a8
louiswu <louiswu@tencent.com>
2018-11-06 16:52:17 +0800

    feat(node): add demo for runtime node6.10 and node8.9

---
+++ Node6.10-ImageCompress/index.js
@@ -0,0 +1,26 @@
+'use strict'
+/**************************************************
+公有云 - 图片压缩
+Tips: 调用腾讯云数据万象接口对图片进行压缩，参考: https://cloud.tencent.com/document/product/460/6925
+***************************************************/
+
+const path = require('path')
+
+exports.main_handler = (event, context, callback) => {
+  let { Records } = event
+  const quality = '80'
+  let url
+
+  if (Records) {
+    url = Records[0].cos.cosObject.url
+  }
+
+  const compressedUrl = path.join(url + '?imageMogr2/rquality/', quality)
+
+  console.log('入参:', event)
+  console.log('图片地址:', url)
+  console.log('图片压缩比:', quality)
+  console.log('压缩后图片地址:', compressedUrl)
+
+  return compressedUrl
+}
