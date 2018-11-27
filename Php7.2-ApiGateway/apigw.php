68c33325416eec49daadf1e3943345035b7cee0d
Masonlu <masonlu@tencent.com>
2018-11-27 16:30:55 +0800

    更新config.json

--- Php7.2-ApiGateway/apigw.php
+++ Php7.2-ApiGateway/apigw.php
@@ -10,11 +10,23 @@ function main_handler($event, $context) {
         print "match method: $req->httpMethod\n";
         print "match path: $req->path\n";
         print "sourceIp: $req->sourceIp";
-        return 'success';
+        $content='API GW Test';
     }
     else {
-        return 'event is not come from api gateway';
+        $content = 'event is not come from api gateway';
     }
+
+    //构造API网关返回值
+    $rep = array(
+    "isBase64Encoded" => false,
+    "statusCode"=> 200,
+    "headers"=> array(
+        "Content-Type"=>"text/html"
+        ),
+    "body"=> $content,
+    );
+    // $rep = json_encode($rep.true)
+    return $rep;
 }
 
 ?>
