a222a87ae767008429bff372e715e7587469426d
louiswu <louiswu@tencent.com>
2019-01-17 16:39:42 +0800

    feat(scf): node8.9增加demo-从apigw返回自定义html

---
+++ Nodejs8.9-CustomHtml/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+
+String.prototype.render = function(variables) {
+  let tpl = this
+  for (let key in variables) {
+    const reg = new RegExp('\\$\\{' + key + '\\}', 'g')
+    tpl = tpl.replace(reg, variables[key])
+  }
+  return tpl
+}
+
+exports.main_handler = async (event, callback, context) => {
+  let html = fs.readFileSync(path.resolve(__dirname, './index.html'), {
+    encoding: 'utf-8'
+  })
+  html = html.render({
+    master: '深圳腾讯科技公司', // 您的名称
+    centralCouplet: '年年有余', // 横批
+    upCouplet: '千年迎新春', // 上联
+    downCouplet: '瑞雪兆丰年' // 下联
+  })
+  return {
+    isBase64Encoded: false,
+    statusCode: 200,
+    headers: { 'Content-Type': 'text/html' },
+    body: html
+  }
+}
