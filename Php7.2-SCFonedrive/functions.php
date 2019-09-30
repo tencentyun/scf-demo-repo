c970a62daf76e0082fbef82813852235c937a89b
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2019-09-30 10:04:14 +0800

    Create functions.php

---
+++ Php7.2-SCFonedrive/functions.php
@@ -0,0 +1,160 @@
+<?php
+function curl_request($url, $data = false, $headers = [])
+{
+    if (!isset($headers['Accept'])) $headers['Accept'] = '*/*';
+    if (!isset($headers['Referer'])) $headers['Referer'] = $url;
+    if (!isset($headers['Content-Type'])) $headers['Content-Type'] = 'application/x-www-form-urlencoded';
+    $sendHeaders = array();
+    foreach ($headers as $headerName => $headerVal) {
+        $sendHeaders[] = $headerName . ': ' . $headerVal;
+    }
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    if ($data !== false) {
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    }
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // 返回获取的输出文本流
+    curl_setopt($ch, CURLOPT_HEADER, 0);         // 将头文件的信息作为数据流输出
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $sendHeaders);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
+function path_format($path)
+{
+    $path = '/' . $path;
+    while (strpos($path, '//') !== FALSE) {
+        $path = str_replace('//', '/', $path);
+    }
+    return $path;
+}
+function size_format($byte)
+{
+    $i = 0;
+    while (abs($byte) >= 1024) {
+        $byte = $byte / 1024;
+        $i++;
+        if ($i == 3) break;
+    }
+    $units = array('B', 'KB', 'MB', 'GB', 'TB');
+    $ret = round($byte, 2);
+    return ($ret . ' ' . $units[$i]);
+}
+function time_format($ISO)
+{
+    $ISO = str_replace('T', ' ', $ISO);
+    $ISO = str_replace('Z', ' ', $ISO);
+    //return $ISO;
+    return date('Y-m-d H:i:s',strtotime($ISO . " UTC"));
+}
+function get_timezone($timezone = '8')
+{
+    $timezones = array( 
+        '-12'=>'Pacific/Kwajalein', 
+        '-11'=>'Pacific/Samoa', 
+        '-10'=>'Pacific/Honolulu', 
+        '-9'=>'America/Anchorage', 
+        '-8'=>'America/Los_Angeles', 
+        '-7'=>'America/Denver', 
+        '-6'=>'America/Mexico_City', 
+        '-5'=>'America/New_York', 
+        '-4'=>'America/Caracas', 
+        '-3.5'=>'America/St_Johns', 
+        '-3'=>'America/Argentina/Buenos_Aires', 
+        '-2'=>'America/Noronha',
+        '-1'=>'Atlantic/Azores', 
+        '0'=>'UTC', 
+        '1'=>'Europe/Paris', 
+        '2'=>'Europe/Helsinki', 
+        '3'=>'Europe/Moscow', 
+        '3.5'=>'Asia/Tehran', 
+        '4'=>'Asia/Baku', 
+        '4.5'=>'Asia/Kabul', 
+        '5'=>'Asia/Karachi', 
+        '5.5'=>'Asia/Calcutta', //Asia/Colombo
+        '6'=>'Asia/Dhaka',
+        '6.5'=>'Asia/Rangoon', 
+        '7'=>'Asia/Bangkok', 
+        '8'=>'Asia/Shanghai', 
+        '9'=>'Asia/Tokyo', 
+        '9.5'=>'Australia/Darwin', 
+        '10'=>'Pacific/Guam', 
+        '11'=>'Asia/Magadan', 
+        '12'=>'Asia/Kamchatka'
+    ); 
+    return $timezones[$timezone];
+}
+function output($body, $statusCode = 200, $isBase64Encoded = false, $headers = ['Content-Type' => 'text/html'])
+{
+    return [
+        'isBase64Encoded' => $isBase64Encoded,
+        'statusCode' => $statusCode,
+        'headers' => $headers,
+        'body' => $body
+    ];
+}
+function message($message, $title = 'Message', $statusCode = 200)
+{
+    return output('<html><meta charset=utf-8><body><h1>' . $title . '</h1><p>' . $message . '</p></body></html>', $statusCode);
+}
+function spurlencode($str,$splite='') {
+    $str = str_replace(' ', '%20',$str);
+    $tmp='';
+    if ($splite!='') {
+        $tmparr=explode($splite,$str);
+        for($x=0;$x<count($tmparr);$x++) {
+            if ($tmparr[$x]!='') $tmp .= $splite . urlencode($tmparr[$x]);
+        }
+    } else {
+        $tmp = urlencode($str);
+    }
+    $tmp = str_replace('%2520', '%20',$tmp);
+    return $tmp;
+}
+function passhidden($path)
+{
+    global $config;
+    $path = str_replace('+','%2B',$path);
+    $path = str_replace('&amp;','&', path_format(urldecode($path)));
+    if ($config['passfile'] != '') {
+        if (substr($path,-1)=='/') $path=substr($path,0,-1);
+        $hiddenpass=gethiddenpass($path,$config['passfile']);
+        if ($hiddenpass != '') {
+            return comppass($hiddenpass);
+        } else {
+            return 1;
+        }
+    } else {
+        return 0;
+    }
+    return 4;
+}
+function gethiddenpass($path,$passfile)
+{
+    $ispassfile = fetch_files(spurlencode(path_format($path . '/' . $passfile),'/'));
+    //echo $path . '<pre>' . json_encode($ispassfile, JSON_PRETTY_PRINT) . '</pre>';
+    if (isset($ispassfile['file'])) {
+        $passwordf=explode("\n",curl_request($ispassfile['@microsoft.graph.downloadUrl']));
+        $password=$passwordf[0];
+        $password=md5($password);
+        return $password;
+    } else {
+        if ($path !== '' ) {
+            $path = substr($path,0,strrpos($path,'/'));
+            return gethiddenpass($path,$passfile);
+        } else {
+            return '';
+        }
+    }
+    return '';
+}
+function comppass($pass) {
+    if ($_POST['password1'] !== '') if (md5($_POST['password1']) === $pass ) return 2;    
+    if ($_COOKIE['password'] !== '') if ($_COOKIE['password'] === $pass ) return 3;
+    return 4;
+}
