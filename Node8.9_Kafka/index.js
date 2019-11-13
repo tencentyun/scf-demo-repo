c16c1172d25deb2be2651d6e5a405ec185cf4cba
Jiachen0417 <44248273+Jiachen0417@users.noreply.github.com>
2019-11-12 19:13:40 -0500

    Add files via upload

--- Node8.9_Kafka/index.js
+++ Node8.9_Kafka/index.js
@@ -1,22 +1,25 @@
 /**************************************************
-公有云 - kafka 往 producer发消息
+Nodejs8.9 - kafka 
 TIPS: 
-1. 云函数需求kafka实例位于相同的vpc内
-参考: 
-1. https://www.npmjs.com/package/kafka-node - kafka Node.js 客户端
-2. https://cloud.tencent.com/product/ckafka/getting-started - 腾讯云 ckafka相关文档
+1. The cloud function and kafka should in same vpc
+   云函数需求kafka实例位于相同的vpc内
+Reference: 
+1. https://www.npmjs.com/package/kafka-node - kafka Node.js 
+2. https://cloud.tencent.com/product/ckafka/getting-started - ckafka document
 ***************************************************/
 
 const kafka = require('kafka-node');
-const client = new kafka.KafkaClient({ kafkaHost: 'xxxx' }); // ckafka host地址，在ckafka基本信息也查看
+const client = new kafka.KafkaClient({ kafkaHost: 'xxxx' }); // The address of ckafka host, you can also check it in the "basic information" of ckafka
+                                                             //ckafka host地址，在ckafka基本信息也查看
 const producer = new kafka.Producer(client)
 
 exports.main_handler = async (event, context, callback) => {
 
   const payloads = [
     { 
-      topic: 'xxx', // topic 名称
-      messages: 'hello world', // 消息内容
+      topic: 'xxx', // The name of the topic
+                    //topic 名称
+      messages: 'hello world', // THe message body
     }
   ]
 
