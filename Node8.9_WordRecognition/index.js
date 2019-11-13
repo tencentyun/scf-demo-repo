377242ed9434fd403a37cff13550238796d0121b
Jiachen0417 <44248273+Jiachen0417@users.noreply.github.com>
2019-11-12 19:15:46 -0500

    Add files via upload

--- Node8.9_WordRecognition/index.js
+++ Node8.9_WordRecognition/index.js
@@ -1,17 +1,20 @@
 /**************************************************
-公有云 - COS上传图片后，调用腾讯云图像智能的接口文字识别
+Node8.9 - WordRecognition
 TIPS: 
-1. 腾讯云图像智能还有其他能力, 请参考api文档
-2. 如若为API网关触发方式, 只是取数据的方式有所不同
-参考: 
-1. https://cloud.tencent.com/document/product/866/17594 - 腾讯云图像智能文档
-2. https://www.npmjs.com/package/tcb-ai - tcb-ai相关文档
+1. Tencent cloud also has other tools to deal with pictures, you can view api documents for more details
+   腾讯云图像智能还有其他能力, 请参考api文档
+2. If triggered by API gateway, the only difference is retrive data
+   如若为API网关触发方式, 只是取数据的方式有所不同
+
+Reference: 
+1. https://cloud.tencent.com/document/product/866/17594 - Tencent cloud image document
+2. https://www.npmjs.com/package/tcb-ai - tcb-ai document
 ***************************************************/
 
 const config = {
-  appId: '', // 请替换为您的腾讯云Appid
-  secretId: '', // 请替换为您的 SecretId
-  secretKey: '', // 请替换为您的 SecretKey
+  appId: '', // Replace it with your Appid please, 请替换为您的腾讯云Appid
+  secretId: '', // Replace it with your SecretId please, 请替换为您的 SecretId
+  secretKey: '', // Replace it with your SecretKey please, 请替换为您的 SecretKey
 }
 
 const { ImageClient } = require("tcb-ai");
@@ -28,10 +31,10 @@ exports.main_handler = async (event, context, callback) => {
     for (let record of event["Records"]) {
       console.log(record);
   
-      // 获取图片地址
+      // Get image's url, 获取图片地址
       const cosObject = record.cos.cosObject;
       const imgUrl = cosObject.url;
-      // 抽取出图片的名称
+      // Extract image's name, 抽取出图片的名称
       console.log("imgUrl is: ", imgUrl);
   
       const res = await imgClient.ocrIdCard({
@@ -45,7 +48,7 @@ exports.main_handler = async (event, context, callback) => {
     }
     return "Success"
   } catch (err) {
-    console.log('错误', err)
+    console.log('Failed', err)
     return "Fail"
   }
 };
