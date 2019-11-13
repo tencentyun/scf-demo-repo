29bb009349e39e5992382d0da18bf848f4f88065
Jiachen0417 <44248273+Jiachen0417@users.noreply.github.com>
2019-11-12 19:18:14 -0500

    Add files via upload

--- Nodejs8.9-AutomatedTesting/index.js
+++ Nodejs8.9-AutomatedTesting/index.js
@@ -1,33 +1,38 @@
 /**************************************************
-公有云 - 拨测函数，并发送告警邮件
+Nodejs8.9-AutomatedTesting
 TIPS: 
-1. CMQ暂时没有node版本的SDK
+1. There is no CMQ SDK for node
+   CMQ暂时没有node版本的SDK
 
-参考: 
-1. https://cloud.tencent.com/document/product/583/19504 - 拨测函数（python版）
-2. https://cloud.tencent.com/document/product/406/5851 - CMQ接口文档
+Reference: 
+1. https://cloud.tencent.com/document/product/583/19504 - Function test（python）
+2. https://cloud.tencent.com/document/product/406/5851 - CMQ interface files
 ***************************************************/
 
 const Capi = require('qcloudapi-sdk')
 const request = require('request-promise')
 
+// The cmq authentication information
 // 使用 cmq 所需的鉴权/配置信息
-const SECRET_ID = 'xxx' // 请替换为您的 SecretId
-const SECRET_KEY = 'xxx' // 请替换为您的 SecretKey
-const CMQ_TOPIC_NAME = 'CMQ_TOPIC_NAME' // 请替换为您的 Topic 名称
-const CMQ_REGION = 'gz' // cmq主题所在地域
+const SECRET_ID = 'xxx' // Replace it with your SecretId , 请替换为您的 SecretId
+const SECRET_KEY = 'xxx' // Replace it with your SecretKey, 请替换为您的 SecretKey
+const CMQ_TOPIC_NAME = 'CMQ_TOPIC_NAME' // Replace it with your Topic name, 请替换为您的 Topic 名称
+const CMQ_REGION = 'gz' // The region of your cmq topic, cmq主题所在地域
 
-// 拨测失败后，告警邮件需要通知的邮箱列表
+// While fails, the email notify list, 拨测失败后，告警邮件需要通知的邮箱列表
 const EMAIL_NOTIFY_LIST = ['******@qq.com', '******@qq.com']
 
+// While fails, the email sending the error message, please replace it with your own email address
 // 拨测失败后，发出告警邮件的邮箱，请根据您自身设置的邮箱地址进行修改
 const FROM_ADDR = '******@qq.com'
 
+// The test url list
 // 拨测网址列表
 const TEST_URL_LIST = ['http://wrong.tencent.com', 'http://www.qq.com']
 
-/**简易CMQ-SDK */
+/**Simple CMQ-SDK */
 function CMQRequestHelper(SecretId, SecretKey) {
+  // Generate the CMQ api
   // CMQ云api构建
   this.requestHelper = new Capi({
     SecretId,
@@ -41,7 +46,7 @@ CMQRequestHelper.prototype.publishMessage = async function(
   topicName,
   msgBody
 ) {
-  if (!this.inited) throw Error('请先实例化CMQRequestHelper~')
+  if (!this.inited) throw Error('Instantiate CMQRequestHelper first please')
   const self = this
   let params = {
     Region: region,
@@ -66,7 +71,7 @@ async function sendCMQ(body) {
       JSON.stringify({
         fromAddr: FROM_ADDR,
         toAddr: toAddr,
-        title: 'Please note: PlayCheck Error 拨测地址异常，请检查',
+        title: 'Please note: PlayCheck Error ',
         body: body
       })
     )
