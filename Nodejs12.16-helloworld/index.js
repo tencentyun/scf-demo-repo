4a1f0bb46f08f3a62aadacda8f6b54d8fd3fdff1
dorasang <sangliu111@126.com>
2020-09-11 17:09:22 +0800

    Update index.js

--- Nodejs12.16-helloworld/index.js
+++ Nodejs12.16-helloworld/index.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.main_handler = async (event, context) => {
+exports.main_handler = async (event, context, callback) => {
     console.log("Hello World")
     console.log(event)
     console.log(event["non-exist"])
